26e8e7f40e73922623bdd2a39990e365abf5a614
srinija landa <srinijalanda@gmail.com>
2023-12-13 15:10:44 +0530

    HOF

--- HOF.js
+++ HOF.js
@@ -44,15 +44,15 @@ console.log(b(1,2));
 
 //HOF and callback
 
-function fName(cb){ //here fName =>HOF and f=>Callback
-    console.log("Hof");
-    cb(); //callback function
-}
-let f=()=>{
-    console.log("call back function");
-}
-//calling the function;
-fName(f);
+// function fName(cb){ //here fName =>HOF and f=>Callback
+//     console.log("Hof");
+//     cb(); //callback function
+// }
+// let f=()=>{
+//     console.log("call back function");
+// }
+// //calling the function;
+// fName(f);
 
 //return an function we callit as HOF
 
@@ -68,15 +68,15 @@ fName(f);
 
 //HOF ON ARRAY
 
-let arr=[3,4,5];
-console.log(arr.length);
-var cb=(e,i,list) =>{
-   // console.log("srinija landa");
-   console.log(e,i,list);
-}
-arr.forEach(cb);
-let result=arr.forEach(cb);
-console.log(result); //returns undefined
+// let arr=[3,4,5];
+// console.log(arr.length);
+// var cb=(e,i,list) =>{
+//    // console.log("srinija landa");
+//    console.log(e,i,list);
+// }
+// arr.forEach(cb);
+// let result=arr.forEach(cb);
+// console.log(result); //returns undefined
 
 //Internal working of forEach
 // function MyfroEach(cb){
@@ -84,10 +84,60 @@ console.log(result); //returns undefined
 // }
 
 //Program Sum of array using ForEach
-let sum=0;
-var cb=(e,i,list)=>{
-    sum+=e;
-}
-arr.forEach(cb);
-console.log(sum);
+// let sum=0;
+// var cb=(e,i,list)=>{
+//     sum+=e;
+// }
+// arr.forEach(cb);
+// console.log(sum);
 
+//Map 
+// var arr=[1,2,3,4,22,45,32,6];
+// let cb=(e,i,l) =>{
+//     console.log(e,i,l);
+//    // return e%2==0? e*2 : e*5;  
+// }
+//let a=arr.map(cb); //here a is an array 
+//console.log(a); //if  return not mentioned 'undefined' of same length;
+
+
+// let cb1=(e,i,l) =>{
+//     //console.log(e,i,l);
+//    return e%2===0 ; //here it created an array [] where it takes the ele which are even numbers
+// }
+// let a1=arr.filter(cb1); //here a is an array 
+// console.log(a1);
+
+ var arr=[1,2,3,4,2,5,2,6];
+ let count=0;
+ let cb=(previous,curr_ele,index) =>{
+    console.log(curr_ele);
+    return previous+curr_ele;
+ }
+ let sum=arr.reduce(cb,0); 
+ //inital value is not mentioned execution start at index 1 taking previous=arr[0];
+ console.log(sum);
+
+ let a=[23,true,5,6,"srinija"]
+ //using keys
+ for(let i in a){ //i datatype is string but works
+    console.log(`index :${i} and element:${a[i]} its datatype ${typeof i}`)
+ }
+
+ //using values
+ for(let j of a){
+    console.log(`element :${j}  its datatype ${typeof j}`)
+ }
+ let student ={
+    name:'srinija landa',
+    single:true,
+    age:21,
+    study:'js',
+    hobby:['eat','sleep','repeat'],
+    quotes:"loveYourSelf"
+ }
+
+ for(let k in student){
+    console.log(`keyName :${k} and its value:${student[k]} its datatype ${typeof student[k]}`)
+
+ }
